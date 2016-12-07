afdf738644d876865a61fa23eb8d00acaccb3710
Sebastian Luke <seb.luke@gmail.com>
2016-12-07 11:14:34 +0200

    Initial Commit

---
+++ JavaImageFilters/src/main/java/com/warptronic/imgfilters/App.java
@@ -0,0 +1,13 @@
+package com.warptronic.imgfilters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
